dfec423f043cd0308a6c2101adba1538a223f446
afeiship <1290657123@qq.com>
2018-06-26 18:26:57 +0800

    issue: rename

---
+++ src/mp-col.vue
@@ -0,0 +1,35 @@
+<template>
+  <view :class="['col', alignClass, spanClass, offsetClass, className]">
+    <slot></slot>
+  </view>
+</template>
+
+<script>
+export default {
+  props: {
+    align: {
+      type: String
+    },
+    span: {
+      type: String
+    },
+    offset: {
+      type: String
+    },
+    className: {
+      type: String
+    }
+  },
+  computed: {
+    alignClass() {
+      return this.align ? `col-${this.align}` : null;
+    },
+    spanClass() {
+      return this.span ? `col-${this.span}` : null;
+    },
+    offsetClass() {
+      return this.offset ? `col-${this.offset}` : null;
+    }
+  }
+};
+</script>
