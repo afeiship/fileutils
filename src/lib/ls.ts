0d649d8a767e44f999ca46b5e7458b645ca534d2
afeiship <1290657123@qq.com>
2023-08-09 19:14:25 +0800

    refactor: ls add defs

--- src/lib/ls.ts
+++ src/lib/ls.ts
@@ -2,9 +2,14 @@ import fs from 'fs';
 import fg from 'fast-glob';
 import type { Options } from 'fast-glob';
 
+const defaults: Options = {
+  dot: true,
+  onlyFiles: true,
+};
+
 const ls = (src: string, options?: Options) => {
   if (!fs.existsSync(src)) throw new Error(`Path ${src} does not exist`);
-  return fg.sync(['**/*'], { cwd: src, dot: true, onlyFiles: true, ...options });
+  return fg.sync(['**/*'], { cwd: src, ...defaults, ...options });
 };
 
 export default ls;
